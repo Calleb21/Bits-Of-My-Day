afc556e21ee1a5c6cd33e3fe5c5de8ba0015cde3
Calleb Camargo <113070078+Calleb21@users.noreply.github.com>
2023-08-01 13:58:18 -0300

    projeto-apollo11

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import './App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+  const [showTopics, setShowTopics] = useState(false);
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+  const toggleTopics = () => {
+    setShowTopics(!showTopics);
+  };
+
+  return (
+    <div>
+      {/* Cabeçalho */}
+      <header className="header">
+        <div className="header-content">
+          {/* Logo e nome */}
+          <div className="header-left">
+            <img src="https://honeysucklecreek.net/images/images_Apollo_11/Apollo_11_patch_vectorised_transp_sm.png" alt="Logo" className="logo" />
+            <h1>Apollo's Legacy</h1>
+          </div>
+        </div>
+
+        {/* Botão para mostrar/ocultar os tópicos */}
+        <button onClick={toggleTopics} className="show-topics-button">
+          <img src="https://cdn-icons-png.flaticon.com/512/463/463292.png" alt="Mostrar Tópicos" />
+        </button>
+      </header>
+
+      {/* Modal */}
+      <Modal
+        isOpen={showTopics}
+        onRequestClose={() => setShowTopics(false)}
+        className="modal"
+        overlayClassName="overlay"
+      >
+        {/* Conteúdo da modal */}
+        <ul className="modal-topics">
+          <li>Tópico 1</li>
+          <li>Tópico 2</li>
+          <li>Tópico 3</li>
+        </ul>
+        <button onClick={() => setShowTopics(false)}>Fechar</button>
+      </Modal>
+    </div>
   );
 }
 
 export default App;
-
-
-
-
-
