f92e5760ab51aa53439efb11980ee70a97d4349f
Calleb Camargo <113070078+Calleb21@users.noreply.github.com>
2023-08-02 14:30:33 -0300

    projeto-apollo11-pt2

--- src/App.js
+++ src/App.js
@@ -16,16 +16,30 @@ function App() {
         <div className="header-content">
           {/* Logo e nome */}
           <div className="header-left">
-            <img src="https://honeysucklecreek.net/images/images_Apollo_11/Apollo_11_patch_vectorised_transp_sm.png" alt="Logo" className="logo" />
+            <img
+              src="https://honeysucklecreek.net/images/images_Apollo_11/Apollo_11_patch_vectorised_transp_sm.png"
+              alt="Logo"
+              className="logo"
+            />
             <h1>Apollo's Legacy</h1>
           </div>
         </div>
 
         {/* Botão para mostrar/ocultar os tópicos */}
         <button onClick={toggleTopics} className="show-topics-button">
-          <img src="https://cdn-icons-png.flaticon.com/512/463/463292.png" alt="Mostrar Tópicos" />
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/463/463292.png"
+            alt="Mostrar Tópicos"
+          />
         </button>
       </header>
+
+      {/* Novo elemento com a imagem de fundo */}
+      <div className="background-image"></div>
+      <div className="background-image1"></div>
+      <div className="background-image2"></div>
+      <div className="background-image3"></div>
+      <div className="background-image4"></div>
 
       {/* Modal */}
       <Modal
@@ -42,6 +56,22 @@ function App() {
         </ul>
         <button onClick={() => setShowTopics(false)}>Fechar</button>
       </Modal>
+
+       {/* Footer */}
+       <footer className="footer">
+        <p>Apollo's Legacy © 2023</p>
+        <div className="social-buttons">
+          <a href="https://instagram.com/calleb_camargo_01?igshid=NTc4MTIwNjQ2YQ==" rel="noopener noreferrer">
+            <button className="social-button instagram">Instagram</button>
+          </a>
+          <a href="https://www.linkedin.com/in/calleb-camargo-682321237" target="_blank" rel="noopener noreferrer">
+            <button className="social-button linkedin">LinkedIn</button>
+          </a>
+          <a href="https://github.com/Calleb21" target="_blank" rel="noopener noreferrer">
+            <button className="social-button github">GitHub</button>
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
