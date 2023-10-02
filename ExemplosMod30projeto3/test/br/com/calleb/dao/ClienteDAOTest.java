e9ff414907a2d7aa891527acfab00d40e33d6a44
Calleb Camargo <113070078+Calleb21@users.noreply.github.com>
2023-10-02 09:03:20 -0300

    projeto-03-ebac-parte-2

--- ExemplosMod30projeto3/test/br/com/calleb/dao/ClienteDAOTest.java
+++ ExemplosMod30projeto3/test/br/com/calleb/dao/ClienteDAOTest.java
@@ -1,8 +1,75 @@
 package br.com.calleb.dao;
 
+import br.com.calleb.domain.Cliente;
+import br.com.calleb.exceptions.DAOException;
+import br.com.calleb.exceptions.MaisDeUmRegistroException;
+import br.com.calleb.exceptions.TableException;
+import br.com.calleb.exceptions.TipoChaveNaoEncontradaException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collection;
+
 /**
  * Description of ClienteDAOTest
  * Created by calle on 25/09/2023.
  */
 public class ClienteDAOTest {
+
+    private final IClienteDAO clienteDAO;
+
+    public ClienteDAOTest() {
+        clienteDAO = new ClienteDAO();
+    }
+
+    @After
+    public void end() throws DAOException {
+        Collection<Cliente> list = clienteDAO.buscarTodos();
+        list.forEach(cli -> {
+            try {
+                clienteDAO.excluir(cli.getCpf());
+            } catch (DAOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Test
+    public void pesquisarCliente() throws MaisDeUmRegistroException, TableException, TipoChaveNaoEncontradaException, DAOException {
+        Cliente cliente = new Cliente();
+        cliente.setCpf(14145245284L);
+        cliente.setNome("Calleb Camargo");
+        cliente.setCidade("Caldas Novas");
+        cliente.setEstado("GO");
+        cliente.setEnd("Rua teste, numero Teste");
+        cliente.setNumero(34);
+        cliente.setTel(2456686357L);
+        clienteDAO.cadastrar(cliente);
+
+        Cliente clienteConsultado = clienteDAO.consultar(cliente.getCpf());
+        Assert.assertNotNull(clienteConsultado);
+
+        clienteDAO.excluir(cliente.getCpf());
+    }
+
+    @Test
+    public void salvarCliente() throws TipoChaveNaoEncontradaException, MaisDeUmRegistroException, TableException, DAOException {
+        Cliente cliente = new Cliente();
+        cliente.setCpf(14145245284L);
+        cliente.setNome("Calleb Camargo");
+        cliente.setCidade("Caldas Novas");
+        cliente.setEstado("GO");
+        cliente.setEnd("Rua teste, numero Teste");
+        cliente.setNumero(34);
+        cliente.setTel(2456686357L);
+        Boolean retorno = clienteDAO.cadastrar(cliente);
+        Assert.assertTrue(retorno);
+
+        Cliente clienteConsultado = clienteDAO.consultar(cliente.getCpf());
+        Assert.assertNotNull(clienteConsultado);
+
+        clienteDAO.excluir(cliente.getCpf());
+    }
 }
